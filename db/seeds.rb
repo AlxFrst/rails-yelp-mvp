6dc80f238cd5f02c2c0031c3c519463127f319b5
Alexandre Forestier <alexandreforestier1107@gmail.com>
2022-05-18 11:24:28 +0200

    en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
